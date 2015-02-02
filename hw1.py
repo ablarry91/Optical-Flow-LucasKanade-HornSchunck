d68a845e4283b1c468bd7e508638b39e4af76b46
Austin Lawrence <ablarry91@gmail.com>
2015-02-02 09:28:57 -0600

    Made a simple box example.  Identified that my A and B matrices are calculated incorrectly - I should not be taking changes over time.

--- hw1.py
+++ hw1.py
@@ -25,11 +25,11 @@ def compareGraphs():
 	# plt.title('Sobel X'), plt.xticks([]), plt.yticks([])
 	# plt.subplot(2,2,4),plt.imshow(sobelY,cmap = 'gray')
 	# plt.title('Sobel Y'), plt.xticks([]), plt.yticks([])
-	plt.ion() #makes it so plots don't block code execution
+	# plt.ion() #makes it so plots don't block code execution
 	plt.imshow(img,cmap = 'gray')
 	plt.scatter(POI[:,0,0],POI[:,0,1])
 	for i in range(len(POI)):
-		plt.arrow(POI[i,0,0],POI[i,0,1],V[i,0]*20,V[i,1]*20)
+		plt.arrow(POI[i,0,0],POI[i,0,1],V[i,0]*5,V[i,1]*5)
 	# plt.arrow(POI[:,0,0],POI[:,0,1],0,-5)
 	plt.show()
 
@@ -85,16 +85,46 @@ def totalGrad(gradX,gradY):
 				pass
 	return grad
 
+def getPOI(xSize, ySize, kernelSize):
+	mean = kernelSize//2
+	xPos = mean
+	yPos = mean
+	xStep = (xSize-mean)//kernelSize
+	yStep = (ySize-mean)//kernelSize
+	length = xStep*yStep
+	POI = np.zeros([length,1,2])
+	count = 0
+	for i in range(yStep):
+		for j in range(xStep):
+			POI[count,0,0] = xPos
+			POI[count,0,1] = yPos
+			xPos += kernelSize
+			count += 1
+		xPos = mean
+		yPos += kernelSize
+	return POI
+
+def buildFlowMap(vX, vY, ptX, ptY):
+	flow = np.zeros([len(vX),len(vY)])
+	count = 0
+	for j in range(len(vY)):
+		for i in range(len(vX)):
+			flow[j,i] = (vX[count]**2+vY[count]**2)**.5
+			count += 1
+	return flow
 # def LK2():
-KERNEL = 3 #must be odd/-
+KERNEL = 7 #must be odd/-
 #get your first image
 count = 0
-fileName = 'office/office.' + str(count) + '.bmp'
+directory = 'box/box.'
+# directory = 'office/office.'
+fileName = directory + str(count) + '.bmp'
 img = cv2.imread(fileName,0)
 img = cv2.GaussianBlur(img,(7,7),0)
 
 #evaluate the first frame's POI
-POI = cv2.goodFeaturesToTrack(img,20,.01,20)
+# POI = cv2.goodFeaturesToTrack(img,20,.01,20)
+POI = getPOI(200,200,KERNEL)
 
 #evaluate the first frame's gradients
 gradXOld = cv2.Sobel(img,cv2.CV_64F,1,0,ksize=5)
@@ -108,8 +138,8 @@ W = gaussianWeight(KERNEL)
 while True:
 	#load the next image
 	count += 1
-	img = cv2.imread('office/office.' + str(count) + '.bmp',0)
-	img = cv2.GaussianBlur(img,(7,7),0)
+	img = cv2.imread(directory + str(count) + '.bmp',0)
+	img = cv2.GaussianBlur(img,(7,7),0)	
 	try:
 		if img.any():
 			# print 'it exists'
@@ -126,7 +156,7 @@ while True:
 
 
 	#evaluate every POI
-	V = np.zeros([20,2])
+	V = np.zeros([(POI.shape)[0],2])
 	for i in range(len(POI)):	
 		#build A
 		A = buildA(gradXNew-gradXOld, gradYNew-gradYOld, POI[i][0][0], POI[i][0][1], KERNEL)
@@ -143,19 +173,23 @@ while True:
 		except:
 			pass
 
-	#update lists
-	gradXOld = gradXNew
-	gradYOld = gradYNew
-	gradOld = gradNew
-	POI = cv2.goodFeaturesToTrack(img,20,.01,20)
-
-	#visualize
-
 	compareGraphs()
 	# time.sleep(1)
 	# time.sleep(2)
 	# plt.close("all")
 	print count
+	break
+
+	#update lists
+	gradXOld = gradXNew
+	gradYOld = gradYNew
+	gradOld = gradNew
+	# POI = cv2.goodFeaturesToTrack(img,20,.01,20)
+	POI = getPOI(200,200,KERNEL)
+
+	#visualize
+
+
 	# break
 
 
@@ -172,5 +206,42 @@ while True:
 
 # showImage(img,'original')
 blur = cv2.GaussianBlur(img,(5,5),0)
+
+def lucas_kanade_np(im1, im2, win=2):
+	assert im1.shape == im2.shape
+	I_x = np.zeros(im1.shape)
+	I_y = np.zeros(im1.shape)
+	I_t = np.zeros(im1.shape)
+	I_x[1:-1, 1:-1] = (im1[1:-1, 2:] - im1[1:-1, :-2]) / 2
+	I_y[1:-1, 1:-1] = (im1[2:, 1:-1] - im1[:-2, 1:-1]) / 2
+	I_t[1:-1, 1:-1] = im1[1:-1, 1:-1] - im2[1:-1, 1:-1]
+	params = np.zeros(im1.shape + (5,)) #Ix2, Iy2, Ixy, Ixt, Iyt
+	params[..., 0] = I_x * I_x # I_x2
+	params[..., 1] = I_y * I_y # I_y2
+	params[..., 2] = I_x * I_y # I_xy
+	params[..., 3] = I_x * I_t # I_xt
+	params[..., 4] = I_y * I_t # I_yt
+	del I_x, I_y, I_t
+	cum_params = np.cumsum(np.cumsum(params, axis=0), axis=1)
+	del params
+	win_params = (cum_params[2 * win + 1:, 2 * win + 1:] -
+				  cum_params[2 * win + 1:, :-1 - 2 * win] -
+				  cum_params[:-1 - 2 * win, 2 * win + 1:] +
+				  cum_params[:-1 - 2 * win, :-1 - 2 * win])
+	del cum_params
+	op_flow = np.zeros(im1.shape + (2,))
+	det = win_params[...,0] * win_params[..., 1] - win_params[..., 2] **2
+	op_flow_x = np.where(det != 0,
+						 (win_params[..., 1] * win_params[..., 3] -
+						  win_params[..., 2] * win_params[..., 4]) / det,
+						 0)
+	op_flow_y = np.where(det != 0,
+						 (win_params[..., 0] * win_params[..., 4] -
+						  win_params[..., 2] * win_params[..., 3]) / det,
+						 0)
+	op_flow[win + 1: -1 - win, win + 1: -1 - win, 0] = op_flow_x[:-1, :-1]
+	op_flow[win + 1: -1 - win, win + 1: -1 - win, 1] = op_flow_y[:-1, :-1]
+	return op_flow
+
 
 # compareGraphs()
